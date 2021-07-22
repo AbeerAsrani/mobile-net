e65f3b0f7088041b672c0d92bf4bced7b64253c3
AbeerAsrani <72503306+AbeerAsrani@users.noreply.github.com>
2021-07-22 13:44:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+}
+function modelLoaded()
+{
+  console.log("model is loaded");
+}
+function preload()
+{
+
+}
+
+function draw()
+{
+  image(video,0,0,250,225);
+  classifier.classify(video,gotResult)
+}
+function gotResult(error,results)
+{
+  if(error)
+   {
+     console.error(error);
+   }
+  else
+  {
+    console.log(results)
+    document.getElementById("object").innerHTML=results[0].label;
+    document.getElementById("accuracy").innerHTML=(results[0].confidence.toFixed (2))*100+"%";
+    
+
+  }
+}
